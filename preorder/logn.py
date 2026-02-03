3d957d9c50c8193b255e37e8c9ef4bf06694290e
Mariam Ahhttouche <mariam.ahhttouche@gmail.com>
2026-02-03 08:00:56 +0100

    correct preorder solution

--- preorder/logn.py
+++ preorder/logn.py
@@ -1,9 +1,8 @@
 def preOrder(self, root):
-        if root.left is None and root.right is None:
+	if root.left is None and root.right is None:
             return [root.data]
-        elif root.left is None and root.right:
-            return [root.right.data] + self.preOrder(root.right) 
-        elif root.right and root.left:
-            return [root.data] + self.preOrder(root.left) + [root.right.data]
-        else:
-            return [root.data] + self.preOrder(root.left) 
+        if root.left is None and root.right:
+            return [root.data] + self.preOrder(root.right) 
+        if root.right and root.left:
+            return [root.data] + self.preOrder(root.left) + self.preOrder(root.right)
+        return [root.data] + self.preOrder(root.left) 
